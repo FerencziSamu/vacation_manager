113fda463fae81188f06129f97d45fecdbb7c6bd
samuf <farkas.david05@gmail.com>
2019-01-07 16:13:43 +0100

    getting ready for docker

---
+++ vacationManager/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.secret_key = b'jz\x8dB\xf3\xeb\n\xe3\x9f\x9c\xf7\x8e\xc3"\x8d\x13\xf2\xb9\xd8QxQ6\xcf'
+    app.run(debug=True)
