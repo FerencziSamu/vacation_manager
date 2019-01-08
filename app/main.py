6ab09d6424af9e6bf211a4b6326cac066e355697
samuf <farkas.david05@gmail.com>
2019-01-08 12:34:53 +0100

    ignore

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     return 'Hello, World!'
 
 
 if __name__ == '__main__':
     app.secret_key = b'jz\x8dB\xf3\xeb\n\xe3\x9f\x9c\xf7\x8e\xc3"\x8d\x13\xf2\xb9\xd8QxQ6\xcf'
-    app.run(debug=True)
+    app.run(host='127.0.0.1', debug=True, port=5000)
