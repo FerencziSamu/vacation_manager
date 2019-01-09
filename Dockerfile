923740785d6bb798ca527b01d04effc5c39ad831
samuf <farkas.david05@gmail.com>
2019-01-09 15:22:29 +0100

    docker runs with database but not connected

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:alpine3.7
-COPY . /app
+ADD . ./app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 5000
-CMD python ./main.py
+EXPOSE 5000:5000
+CMD python ./app/main.py
--- app/main.py
+++ app/main.py
@@ -1,13 +1,53 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'postgres',
+    'host': '0.0.0.0',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+db.drop_all()
+db.create_all()
+Admin = User('Samu', 'samu77@freemail.hu')
+Admine = User('Samue', 'esamu77@freemail.hu')
+db.session.add(Admin)
+db.session.add(Admine)
+db.session.commit()
 
 
 @app.route('/')
+def index():
+    return 'INDEX'
+
+
+@app.route('/hello')
 def hello():
     return 'Hello, World!'
 
 
 if __name__ == '__main__':
     app.secret_key = b'jz\x8dB\xf3\xeb\n\xe3\x9f\x9c\xf7\x8e\xc3"\x8d\x13\xf2\xb9\xd8QxQ6\xcf'
-    app.run(host='127.0.0.1', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
